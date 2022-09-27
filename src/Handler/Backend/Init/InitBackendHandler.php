0ff18ce159fbfc727e86df55f49bc3b2b1d3bd22
Roman Bracinik <roman.bracinik@keboola.com>
2022-09-27 15:40:00 +0200

    working initBackend with tests

--- src/Handler/Backend/Init/InitBackendHandler.php
+++ src/Handler/Backend/Init/InitBackendHandler.php
@@ -2,13 +2,54 @@
 
 namespace Keboola\StorageDriver\BigQuery\Handler\Backend\Init;
 
+use Keboola\StorageDriver\Credentials\BigQueryCredentials;
+use Keboola\StorageDriver\Shared\Driver\Exception\Exception;
+use Google\Cloud\ResourceManager\V3\FoldersClient;
 use Google\Protobuf\Internal\Message;
+use Keboola\StorageDriver\Command\Backend\InitBackendCommand;
+use Keboola\StorageDriver\Command\Backend\InitBackendResponse;
 use Keboola\StorageDriver\Contract\Driver\Command\DriverCommandHandlerInterface;
 
 final class InitBackendHandler implements DriverCommandHandlerInterface
 {
-    public function __invoke(Message $credentials, Message $command, array $features): ?Message
-    {
-        // TODO: Implement __invoke() method.
+    /**
+     * @inheritDoc
+     * @param BigQueryCredentials $credentials
+     */
+    public function __invoke(
+        Message $credentials,
+        Message $command,
+        array $features
+    ): ?Message {
+        assert($credentials instanceof BigQueryCredentials);
+        assert($command instanceof InitBackendCommand);
+
+        $foldersClient = new FoldersClient([
+            'credentials' => [
+                'type' =>$credentials->getType(),
+                'project_id' =>$credentials->getProjectId(),
+                'private_key_id' =>$credentials->getPrivateKeyId(),
+                'private_key' =>$credentials->getPrivateKey(),
+                'client_email' =>$credentials->getClientEmail(),
+                'client_id' =>$credentials->getClientId(),
+                'auth_uri' =>$credentials->getAuthUri(),
+                'token_uri' =>$credentials->getTokenUri(),
+                'auth_provider_x509_cert_url' =>$credentials->getAuthProviderX509CertUrl(),
+                'client_x509_cert_url' =>$credentials->getClientX509CertUrl(),
+            ],
+        ]);
+
+        try {
+            $formattedName = $foldersClient->folderName($credentials->getFolderId());
+            $folderPermissions = $foldersClient->testIamPermissions($formattedName, ['resourcemanager.projects.create']);
+        } finally {
+            $foldersClient->close();
+        }
+
+        if (count($folderPermissions->getPermissions()) === 0) {
+            throw new Exception('Missing rights "resourcemanager.projects.create" for service account.');
+        }
+
+        return new InitBackendResponse();
     }
 }
