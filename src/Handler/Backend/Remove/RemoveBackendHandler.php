def0f6e0319de85b272b03cceffc428a57716f4f
Roman Bracinik <roman.bracinik@keboola.com>
2022-09-27 16:11:53 +0200

    Remove backend and test

---
+++ src/Handler/Backend/Remove/RemoveBackendHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\BigQuery\Handler\Backend\Remove;
+
+use Google\Protobuf\Internal\Message;
+use Keboola\StorageDriver\Command\Backend\RemoveBackendCommand;
+use Keboola\StorageDriver\Contract\Driver\Command\DriverCommandHandlerInterface;
+use Keboola\StorageDriver\Credentials\BigQueryCredentials;
+
+final class RemoveBackendHandler implements DriverCommandHandlerInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function __invoke(
+        Message $credentials,
+        Message $command,
+        array $features
+    ): ?Message {
+        assert($credentials instanceof BigQueryCredentials);
+        assert($command instanceof RemoveBackendCommand);
+
+        return null;
+    }
+}
