1d95daf700361241a83d65a383cad6ae9ef8c8ff
Roman Bracinik <roman.bracinik@keboola.com>
2022-10-10 09:34:22 +0200

    Add BQ Driver client

---
+++ src/BigQueryDriverClient.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\BigQuery;
+
+use Google\Protobuf\Internal\Message;
+use Keboola\StorageDriver\BigQuery\Handler\Backend\Init\InitBackendHandler;
+use Keboola\StorageDriver\BigQuery\Handler\Backend\Remove\RemoveBackendHandler;
+use Keboola\StorageDriver\Command\Backend\InitBackendCommand;
+use Keboola\StorageDriver\Command\Backend\RemoveBackendCommand;
+use Keboola\StorageDriver\Contract\Driver\ClientInterface;
+use Keboola\StorageDriver\Contract\Driver\Command\DriverCommandHandlerInterface;
+use Keboola\StorageDriver\Credentials\BigQueryCredentials;
+use Keboola\StorageDriver\Shared\Driver\Exception\CommandNotSupportedException;
+
+class BigQueryDriverClient implements ClientInterface
+{
+    public function runCommand(Message $credentials, Message $command, array $features): ?Message
+    {
+        assert($credentials instanceof BigQueryCredentials);
+        $manager = new GCPClientManager();
+        $handler = $this->getHandler($command, $manager);
+
+        return $handler(
+            $credentials,
+            $command,
+            $features
+        );
+    }
+
+    private function getHandler(Message $command, GCPClientManager $manager): DriverCommandHandlerInterface
+    {
+        switch (true) {
+            case $command instanceof InitBackendCommand:
+                return new InitBackendHandler($manager);
+            case $command instanceof RemoveBackendCommand:
+                return new RemoveBackendHandler();
+        }
+
+        throw new CommandNotSupportedException(get_class($command));
+    }
+}
