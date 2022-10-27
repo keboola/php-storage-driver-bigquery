4b25817b05509760891b7c3213d41cc3478601f3
Roman Bracinik <roman.bracinik@keboola.com>
2022-10-27 16:48:05 +0200

    Create drop table

---
+++ src/Handler/Table/Drop/DropTableHandler.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\BigQuery\Handler\Table\Drop;
+
+use Google\Protobuf\Internal\Message;
+use Keboola\StorageDriver\BigQuery\GCPClientManager;
+use Keboola\StorageDriver\Command\Table\DropTableCommand;
+use Keboola\StorageDriver\Contract\Driver\Command\DriverCommandHandlerInterface;
+use Keboola\StorageDriver\Credentials\GenericBackendCredentials;
+
+class DropTableHandler implements DriverCommandHandlerInterface
+{
+    public GCPClientManager $clientManager;
+
+    public function __construct(GCPClientManager $clientManager)
+    {
+        $this->clientManager = $clientManager;
+    }
+
+    /**
+     * @inheritDoc
+     * @param GenericBackendCredentials $credentials
+     * @param DropTableCommand $command
+     */
+    public function __invoke(
+        Message $credentials,
+        Message $command,
+        array $features
+    ): ?Message {
+        assert($credentials instanceof GenericBackendCredentials);
+        assert($command instanceof DropTableCommand);
+
+        // validate
+        assert($command->getPath()->count() === 1, 'DropTableCommand.path is required and size must equal 1');
+        assert($command->getTableName() !== '', 'DropTableCommand.tableName is required');
+
+        $bqClient = $this->clientManager->getBigQueryClient($credentials);
+        /** @var string $datasetName */
+        $datasetName = $command->getPath()[0]; // bucket name
+        $dateset = $bqClient->dataset($datasetName);
+        $table = $dateset->table($command->getTableName());
+
+        // drop table
+        try {
+            $table->delete();
+        } catch (\Throwable $e) {
+            if (!$command->getIgnoreErrors()) {
+                throw $e;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ tests/functional/UseCase/Table/CreateDropTableTest.php
@@ -0,0 +1,135 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\FunctionalTests\UseCase\Table;
+
+use Google\Protobuf\Internal\GPBType;
+use Google\Protobuf\Internal\RepeatedField;
+use Keboola\Datatype\Definition\Bigquery;
+use Keboola\StorageDriver\BigQuery\Handler\Table\Create\CreateTableHandler;
+use Keboola\StorageDriver\BigQuery\Handler\Table\Drop\DropTableHandler;
+use Keboola\StorageDriver\Command\Bucket\CreateBucketResponse;
+use Keboola\StorageDriver\Command\Table\CreateTableCommand;
+use Keboola\StorageDriver\Command\Table\DropTableCommand;
+use Keboola\StorageDriver\Credentials\GenericBackendCredentials;
+use Keboola\StorageDriver\FunctionalTests\BaseCase;
+use Keboola\TableBackendUtils\Column\Bigquery\BigqueryColumn;
+use Keboola\TableBackendUtils\Table\Bigquery\BigqueryTableReflection;
+
+class CreateDropTableTest extends BaseCase
+{
+    protected CreateBucketResponse $bucketResponse;
+
+    private GenericBackendCredentials $projectCredentials;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->cleanTestProject();
+
+        [$projectCredentials, $projectResponse] = $this->createTestProject();
+        $this->projectCredentials = $projectCredentials;
+
+        $this->bucketResponse = $this->createTestBucket($projectCredentials);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->cleanTestProject();
+    }
+
+    public function testCreateTable(): void
+    {
+        $tableName = md5($this->getName()) . '_Test_table';
+        $bucketDatasetName = $this->bucketResponse->getCreateBucketObjectName();
+
+        // CREATE TABLE
+        $handler = new CreateTableHandler($this->clientManager);
+
+        $path = new RepeatedField(GPBType::STRING);
+        $path[] = $bucketDatasetName;
+        $columns = new RepeatedField(GPBType::MESSAGE, CreateTableCommand\TableColumn::class);
+        $columns[] = (new CreateTableCommand\TableColumn())
+            ->setName('id')
+            ->setType(Bigquery::TYPE_INT64);
+        $columns[] = (new CreateTableCommand\TableColumn())
+            ->setName('name')
+            ->setType(Bigquery::TYPE_STRING)
+            ->setLength('50')
+            ->setNullable(true)
+            ->setDefault("'Some Default'");
+        $columns[] = (new CreateTableCommand\TableColumn())
+            ->setName('large')
+            ->setType(Bigquery::TYPE_BIGNUMERIC)
+            ->setLength('76,38')
+            ->setDefault('185.554');
+        $command = (new CreateTableCommand())
+            ->setPath($path)
+            ->setTableName($tableName)
+            ->setColumns($columns);
+        $response = $handler(
+            $this->projectCredentials,
+            $command,
+            []
+        );
+        $this->assertNull($response);
+
+        $table = new BigqueryTableReflection(
+            $this->clientManager->getBigQueryClient($this->projectCredentials),
+            $bucketDatasetName,
+            $tableName
+        );
+
+        /** @var BigqueryColumn[] $columns */
+        $columns = iterator_to_array($table->getColumnsDefinitions());
+        $this->assertCount(3, $columns);
+
+        // check column ID
+        $column = $columns[0];
+        $this->assertSame('id', $column->getColumnName());
+        $columnDef = $column->getColumnDefinition();
+        $this->assertSame(Bigquery::TYPE_INT64, $columnDef->getType());
+        $this->assertFalse($columnDef->isNullable());
+        $this->assertNull($columnDef->getDefault());
+
+        // check column NAME
+        $column = $columns[1];
+        $this->assertSame('name', $column->getColumnName());
+        $columnDef = $column->getColumnDefinition();
+        $this->assertSame(Bigquery::TYPE_STRING, $columnDef->getType());
+        $this->assertSame('50', $columnDef->getLength());
+        $this->assertTrue($columnDef->isNullable());
+        $this->assertSame("'Some Default'", $columnDef->getDefault());
+
+        // check column LARGE
+        $column = $columns[2];
+        $this->assertSame('large', $column->getColumnName());
+        $columnDef = $column->getColumnDefinition();
+        $this->assertSame(Bigquery::TYPE_BIGNUMERIC, $columnDef->getType());
+        $this->assertSame('76,38', $columnDef->getLength());
+        $this->assertFalse($columnDef->isNullable());
+        $this->assertSame('185.554', $columnDef->getDefault());
+
+        // DROP TABLE
+        $handler = new DropTableHandler($this->clientManager);
+        $command = (new DropTableCommand())
+            ->setPath($path)
+            ->setTableName($tableName);
+
+        $handler(
+            $this->projectCredentials,
+            $command,
+            []
+        );
+
+        $bqClient = $this->clientManager->getBigQueryClient($this->projectCredentials);
+
+        $bucket = $bqClient->dataset($bucketDatasetName);
+        $this->assertTrue($bucket->exists());
+
+        $table = $bucket->table($tableName);
+        $this->assertFalse($table->exists());
+    }
+}
