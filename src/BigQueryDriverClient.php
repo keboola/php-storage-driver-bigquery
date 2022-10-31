8879fdc90d2af086895db2c2dcd29b043e93fb09
Roman Bracinik <roman.bracinik@keboola.com>
2022-10-31 09:51:24 +0100

    add commands to client

--- src/BigQueryDriverClient.php
+++ src/BigQueryDriverClient.php
@@ -11,12 +11,16 @@ use Keboola\StorageDriver\BigQuery\Handler\Bucket\Create\CreateBucketHandler;
 use Keboola\StorageDriver\BigQuery\Handler\Bucket\Drop\DropBucketHandle;
 use Keboola\StorageDriver\BigQuery\Handler\Project\Create\CreateProjectHandler;
 use Keboola\StorageDriver\BigQuery\Handler\Project\Drop\DropProjectHandler;
+use Keboola\StorageDriver\BigQuery\Handler\Table\Create\CreateTableHandler;
+use Keboola\StorageDriver\BigQuery\Handler\Table\Drop\DropTableHandler;
 use Keboola\StorageDriver\Command\Backend\InitBackendCommand;
 use Keboola\StorageDriver\Command\Backend\RemoveBackendCommand;
 use Keboola\StorageDriver\Command\Bucket\CreateBucketCommand;
 use Keboola\StorageDriver\Command\Bucket\DropBucketCommand;
 use Keboola\StorageDriver\Command\Project\CreateProjectCommand;
 use Keboola\StorageDriver\Command\Project\DropProjectCommand;
+use Keboola\StorageDriver\Command\Table\CreateTableCommand;
+use Keboola\StorageDriver\Command\Table\DropTableCommand;
 use Keboola\StorageDriver\Contract\Driver\ClientInterface;
 use Keboola\StorageDriver\Contract\Driver\Command\DriverCommandHandlerInterface;
 use Keboola\StorageDriver\Credentials\GenericBackendCredentials;
@@ -52,6 +56,10 @@ class BigQueryDriverClient implements ClientInterface
                 return new CreateBucketHandler($manager);
             case $command instanceof DropBucketCommand:
                 return new DropBucketHandle($manager);
+            case $command instanceof CreateTableCommand:
+                return new CreateTableHandler($manager);
+            case $command instanceof DropTableCommand:
+                return new DropTableHandler($manager);
         }
 
         throw new CommandNotSupportedException(get_class($command));
