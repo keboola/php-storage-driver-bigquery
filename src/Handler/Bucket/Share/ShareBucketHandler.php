c80fec881f17ae601ead42f32e44279af20139dc
Roman Bracinik <roman.bracinik@keboola.com>
2022-12-14 13:06:51 +0100

    fix asserts

--- src/Handler/Bucket/Share/ShareBucketHandler.php
+++ src/Handler/Bucket/Share/ShareBucketHandler.php
@@ -37,7 +37,7 @@ final class ShareBucketHandler implements DriverCommandHandlerInterface
 
         assert($command->getSourceProjectId() !== '', 'ShareBucketCommand.sourceProjectId must be filled in');
         assert(
-            str_contains($command->getSourceProjectId(), '/') === true,
+            str_contains($command->getSourceProjectId(), '/') === false,
             'ShareBucketCommand.sourceProjectId cannot contain "/"'
         );
         assert(
@@ -49,7 +49,7 @@ final class ShareBucketHandler implements DriverCommandHandlerInterface
             'ShareBucketCommand.sourceBucketObjectName must be filled in'
         );
         assert(
-            str_contains($command->getSourceProjectId(), '/') === true,
+            str_contains($command->getSourceProjectId(), '/') === false,
             'ShareBucketCommand.sourceBucketObjectName cannot contain "/"'
         );
 
