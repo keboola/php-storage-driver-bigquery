522473ebecdc084659b866c54ae40a0b03f0d277
Jiri Semmler <jirka.semmler@gmail.com>
2023-10-10 00:39:45 +0200

    BIG-168 test retry

---
+++ tests/functional/UseCase/Workspace/ServiceAccountRetryTest.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\FunctionalTests\UseCase\Workspace;
+
+use Google\Cloud\ResourceManager\V3\Project;
+use Google\Cloud\ResourceManager\V3\ProjectsClient;
+use Google\Service\Exception;
+use Google\Service\Iam;
+use Google_Service_Iam_CreateServiceAccountRequest;
+use Keboola\StorageDriver\BigQuery\GCPClientManager;
+use Keboola\StorageDriver\BigQuery\NameGenerator;
+use Keboola\StorageDriver\FunctionalTests\BaseCase;
+use Keboola\StorageDriver\Shared\Driver\Exception\Exception as CommonDriverException;
+
+class ServiceAccountRetryTest extends BaseCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->cleanTestProject();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->cleanTestProject();
+    }
+
+    public function testCreateManyServiceACounts(): void
+    {
+        $credentials = $this->getCredentials();
+        $projectsClient = $this->clientManager->getProjectClient($credentials);
+        $iamClient = $this->clientManager->getIamClient($credentials);
+
+        $stackPrefix = (string) getenv('BQ_STACK_PREFIX');
+        $folderId = (string) getenv('BQ_FOLDER_ID');
+
+        $nameGenerator = new NameGenerator($stackPrefix);
+
+        $projectId = $nameGenerator->createProjectId('-retry-' . time());
+
+        $projectCreateResult = $this->createProject($projectsClient, $folderId, $projectId);
+        $projectName = $projectCreateResult->getName();
+
+        $tooManyRequestsTested = false;
+        $namePrefix = 'sa-' . time();
+        // using very low initial delay to test that the retry will fail. catching the exception will happen later
+        $iamClient->getClient()->setConfig(
+            'retry',
+            ['retries' => 3, 'factor' => 1.1, 'initial_delay' => 5, 'jitter' => 0.1]
+        );
+        for ($i = 1; $i < 10; $i++) {
+            $projectServiceAccountId = $nameGenerator->createProjectServiceAccountId($namePrefix . '-' . $i);
+            try {
+                $this->createServiceAccount($iamClient, $projectServiceAccountId, $projectName);
+            } catch (Exception $e) {
+                $this->assertEquals(429, $e->getCode());
+                $tooManyRequestsTested = true;
+                break;
+            }
+        }
+        if (!$tooManyRequestsTested) {
+            $this->fail('Too many requests not tested');
+        }
+
+        // using default retry setting to test that the retry will succeed
+        $iamClient->getClient()->setConfig('retry', GCPClientManager::DEFAULT_RETRY_SETTINGS);
+        $namePrefix = 'sa-' . time();
+        for ($i = 1; $i < 10; $i++) {
+            $projectServiceAccountId = $nameGenerator->createProjectServiceAccountId($namePrefix . '-' . $i);
+            try {
+                $this->createServiceAccount($iamClient, $projectServiceAccountId, $projectName);
+            } catch (Exception $e) {
+                $this->assertEquals(429, $e->getCode());
+                $this->fail('This should not happen');
+            }
+        }
+    }
+
+    private function createServiceAccount(Iam $iamService, string $projectServiceAccountId, string $projectName): void
+    {
+        $serviceAccountsService = $iamService->projects_serviceAccounts;
+        $createServiceAccountRequest = new Google_Service_Iam_CreateServiceAccountRequest();
+
+        $createServiceAccountRequest->setAccountId($projectServiceAccountId);
+        $serviceAccountsService->create($projectName, $createServiceAccountRequest);
+    }
+
+    // custom project creation because we need project name, should be cleaned by cleanTestProject()
+    private function createProject(ProjectsClient $projectsClient, string $folderId, string $projectId): Project
+    {
+        $project = new Project();
+
+        $project->setParent('folders/' . $folderId);
+        $project->setProjectId($projectId);
+        $project->setDisplayName($projectId);
+
+        $operationResponse = $projectsClient->createProject($project);
+        $operationResponse->pollUntilComplete();
+        if ($operationResponse->operationSucceeded()) {
+            /** @var Project $projectCreateResult */
+            $projectCreateResult = $operationResponse->getResult();
+        } else {
+            $error = $operationResponse->getError();
+            assert($error !== null);
+            throw new CommonDriverException($error->getMessage(), $error->getCode());
+        }
+
+        return $projectCreateResult;
+    }
+}
