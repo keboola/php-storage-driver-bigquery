77d552308961285d95a693e50d7734cb2898a2a1
zajca <zajca@zajca.cz>
2023-10-18 13:33:32 +0200

    update retry on BigQueryClientHandler

--- src/BigQueryClientHandler.php
+++ src/BigQueryClientHandler.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Keboola\StorageDriver\BigQuery;
 
+use Google\Service\Exception as GoogleServiceException;
+use Google\Task\Runner;
 use GuzzleHttp\Client;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use Retry\BackOff\FixedBackOffPolicy;
-use Retry\Policy\SimpleRetryPolicy;
+use Retry\BackOff\UniformRandomBackOffPolicy;
+use Retry\Policy\CallableRetryPolicy;
 use Retry\RetryProxy;
+use Throwable;
 
 class BigQueryClientHandler
 {
@@ -22,8 +25,26 @@ class BigQueryClientHandler
 
     public function __invoke(RequestInterface $request): ResponseInterface
     {
-        $retryPolicy = new SimpleRetryPolicy(10);
-        $proxy = new RetryProxy($retryPolicy, new FixedBackOffPolicy());
+        $retryCount = 0;
+        $retryPolicy = new CallableRetryPolicy(function (Throwable $e) use (&$retryCount) {
+            $retryCount++;
+            if ($e instanceof GoogleServiceException) {
+                $retryStrategy = GCPClientManager::RETRY_MAP[$e->getCode()] ?? Runner::TASK_RETRY_NEVER;
+
+                if (!empty($e->getErrors()) &&
+                    isset($e->getErrors()[0]['reason'], GCPClientManager::RETRY_MAP[$e->getErrors()[0]['reason']])
+                ) {
+                    $retryStrategy = GCPClientManager::RETRY_MAP[$e->getErrors()[0]['reason']];
+                }
+                return match ($retryStrategy) {
+                    Runner::TASK_RETRY_ALWAYS => true,
+                    Runner::TASK_RETRY_ONCE => $retryCount === 1,
+                    default => false
+                };
+            }
+            return false;
+        }, 20);
+        $proxy = new RetryProxy($retryPolicy, new UniformRandomBackOffPolicy());
 
         /** @var ResponseInterface $result */
         $result = $proxy->call(function () use ($request) {
