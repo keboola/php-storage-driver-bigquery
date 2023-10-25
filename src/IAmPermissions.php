aa89789c539a81278cd39c7c0622892996f3e2ad
Roman Bracinik <roman.bracinik@keboola.com>
2023-10-25 15:23:45 +0200

    add permission to workspace service acc roles/bigquery.readSessionUser

--- src/IAmPermissions.php
+++ src/IAmPermissions.php
@@ -12,5 +12,6 @@ final class IAmPermissions
     public const ROLES_BIGQUERY_DATA_VIEWER = 'roles/bigquery.dataViewer';
     public const ROLES_BIGQUERY_JOB_USER = 'roles/bigquery.jobUser';
     public const ROLES_IAM_SERVICE_ACCOUNT_CREATOR = 'roles/iam.serviceAccountCreator';
+    public const ROLES_BIGQUERY_READ_SESSION_USER = 'roles/bigquery.readSessionUser';
     public const ROLES_OWNER = 'roles/owner';
 }
