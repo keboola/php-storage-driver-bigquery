15f1c3f9fc9330cbe6a49775148473a6ccc3dea6
Roman Bracinik <roman.bracinik@keboola.com>
2023-12-14 14:09:07 +0100

    Introduce new policy filter

---
+++ src/Handler/Project/Drop/PolicyFilter.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\BigQuery\Handler\Project\Drop;
+
+class PolicyFilter
+{
+    public static function removeServiceAccFromBucketPolicy(array $policy, string $serviceAccountEmail): array
+    {
+    }
+}
+
---
+++ tests/unit/Project/Drop/PolicyFilterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\UnitTests\Project\Drop;
+
+use Generator;
+use Keboola\StorageDriver\BigQuery\Handler\Project\Drop\PolicyFilter;
+use PHPUnit\Framework\TestCase;
+
+class PolicyFilterTest extends TestCase
+{
+}
