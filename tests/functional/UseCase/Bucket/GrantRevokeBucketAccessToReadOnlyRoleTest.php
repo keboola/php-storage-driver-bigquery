ff44731e0dd01a6f4383a7967a5a93f783c12c52
Roman Bracinik <roman.bracinik@keboola.com>
2025-08-06 09:42:44 +0200

    fix changed err msg

--- tests/functional/UseCase/Bucket/GrantRevokeBucketAccessToReadOnlyRoleTest.php
+++ tests/functional/UseCase/Bucket/GrantRevokeBucketAccessToReadOnlyRoleTest.php
@@ -329,7 +329,7 @@ class GrantRevokeBucketAccessToReadOnlyRoleTest extends BaseCase
             $this->fail('Should not be able to register bucket from another region.');
         } catch (InvalidArgumentException $e) {
             $this->assertSame(
-                'The selected primary location "us" is not ready to use or not supported in the listing.',
+                'Listing region "eu" must be the same as linked dataset region "us".',
                 $e->getMessage(),
             );
             $this->assertSame(3000, $e->getCode());
